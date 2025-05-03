338d6e8fc6d37bfd6a62db1bdc8fcc2e47faab80
sergio <sergio.medina@estudiante.uacm.edu.mx>
2025-05-03 16:50:15 -0600

    Validaciones más seguras en el modelo Plato

--- src/main/java/restaurante/com/model/Plato.java
+++ src/main/java/restaurante/com/model/Plato.java
@@ -3,6 +3,7 @@ package restaurante.com.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @Entity
@@ -22,6 +23,7 @@ public class Plato {
     private String nombre;
 
     @NotNull(message = "El precio es obligatorio")
+    @Positive(message = "El precio debe ser mayor a cero")
     @Column(nullable = false)
     private Double precio;
 
