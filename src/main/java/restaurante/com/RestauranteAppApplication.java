ea0d094468ed58829500a2ebfc4691b79f9d43ee
sergio <sergio.medina@estudiante.uacm.edu.mx>
2025-05-03 17:22:39 -0600

    Se habilita Spring Retry en la app principal

--- src/main/java/restaurante/com/RestauranteAppApplication.java
+++ src/main/java/restaurante/com/RestauranteAppApplication.java
@@ -3,10 +3,12 @@ package restaurante.com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableRetry
 public class RestauranteAppApplication {
 
 	public static void main(String[] args) {
