e067acb78d367ae3ecdbb771a11d6a0770f97dba
sergio <sergio.medina@estudiante.uacm.edu.mx>
2025-05-03 16:57:29 -0600

    Se habilito el CORS restringido en RestauranteAppApplication

--- src/main/java/restaurante/com/RestauranteAppApplication.java
+++ src/main/java/restaurante/com/RestauranteAppApplication.java
@@ -2,6 +2,9 @@ package restaurante.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestauranteAppApplication {
@@ -9,5 +12,16 @@ public class RestauranteAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestauranteAppApplication.class, args);
 	}
-
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				        .allowedOrigins("http://localhost:4200") 
+				        .allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
 }
