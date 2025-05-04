218110f72b672ec9de3d79baef9eb2dfa9b064c8
sergio <sergio.medina@estudiante.uacm.edu.mx>
2025-05-03 18:06:42 -0600

    Se agregaron los getters y setters en la clase Usuario.java

--- src/main/java/restaurante/com/model/Usuario.java
+++ src/main/java/restaurante/com/model/Usuario.java
@@ -33,6 +33,37 @@ public class Usuario {
             joinColumns = @JoinColumn(name = "usuario_id"),
             inverseJoinColumns = @JoinColumn(name = "rol_id"))
     private Set<Rol> roles;
-    
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Rol> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Rol> roles) {
+		this.roles = roles;
+	} 
 
 }
